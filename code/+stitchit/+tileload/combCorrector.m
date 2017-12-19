944045b6de56582da3426c8d6ef270eaed82ddfc
Fouga <natalia.chicherova@gmail.com>
2017-12-19 14:07:01 +0100

    Excluded darkfield correction model. Only flatfield is now correted Additional brightness correction

--- code/+stitchit/+tileload/combCorrector.m
+++ code/+stitchit/+tileload/combCorrector.m
@@ -1,4 +1,3 @@
-
 function im = combCorrector(im,sectionDir,coords,userConfig)
     % crops tiles for stitchit tileLoad
     %
@@ -28,35 +27,12 @@ function im = combCorrector(im,sectionDir,coords,userConfig)
     
     % if finds txt for shift - do the rows correction, otherwise Robs
     % corrections
-    [TxTflag, Image_shift,stripe_size]  = check_rowsShiftTxt(coords,userConfig);
-    if TxTflag
-        im = correct_rowsShift(im,Image_shift,stripe_size);
-    else
+    avDir = [userConfig.subdir.rawDataDir,filesep,userConfig.subdir.averageDir];
+    if ~exist([avDir '/MakeL1Correction.txt'])
         im = correct_phases(im,sectionDir,coords,userConfig);
+    else
+        im = correct_rowsShift(im,coords,userConfig);
     end
-    
-    
-    function [TxTflag, Image_shift,stripe_size]  = check_rowsShiftTxt(coords,userConfig)
-        filename_shift = fullfile(userConfig.subdir.rawDataDir, userConfig.subdir.averageDir, '/Shifts_per_tile.txt');
-        if exist(filename_shift)
-            disp('Correcting combo artifact with L1-norm')
-            TxTflag = 1;
-            fid = fopen(filename_shift, 'r');
-            C = textscan(fid, '%s', [9 Inf]);
-            A = fscanf(fid,'%d',[9 Inf]);
-            fclose(fid);
-            
-            ind = find(A(1,:)==coords(1) & A(2,:)==coords(2));
-            Image_shift = A(4:6,ind);
-            stripe_size = A(7:end,ind);
-        else
-            TxTflag = 0;
-            Image_shift = 0;
-            stripe_size = 0;
-        end
-    
-    
-    
     
     
     function im = correct_phases(im,sectionDir,coords,userConfig)
@@ -101,40 +77,98 @@ function im = combCorrector(im,sectionDir,coords,userConfig)
         end
 
         
-    function IM = correct_rowsShift(im,Image_shift,stripe_size)
-        IM = uint16(zeros(size(im)));
-        for i = 1:size(im,3)
-            Im = im(:,:,i);
-            Im_shift = Image_shift(:,i);
-            Im_stripe_size = stripe_size(:,i);
-            start_stripe = 0;
-            for S = 1:length(Im_stripe_size)
-                stripes = Im_stripe_size(S);% no
-                shift = Im_shift(S);    
-                % take only part of the tile image due to different shifts
-                im_stripe = Im(:,start_stripe+1:stripes);
-                % shift to the left or right
-                if shift>=0
-                    % shift to the left
-                    im1_shift = im_stripe;
-                    for rows = 2:2:size(im1_shift,1)
-                        im1_shift(rows,shift+1:end) = im_stripe(rows,1:end-shift);
+    function imShift = correct_rowsShift(im,coords,userConfig)
+        disp('L1 comb correction \n');
+        imShift = uint16(zeros(size(im)));
+        avDir = [userConfig.subdir.rawDataDir,filesep,userConfig.subdir.averageDir];
+        A=load([avDir '/MakeL1Correction.txt']);
+        l1 = A(1,4);
+        ind = find(A(:,1)==coords(1) & A(:,2)==coords(2));
+        shiftPatches = A(ind,5:end);
+        [M N P] = size(im);
+        q = 1:floor(N/l1);
+        sizVert = [1,l1*q];
+        IMall = cell(floor(N/l1)+1,1);
+        for imS = 1:P
+            rgbIm = im(:,:,imS);
+            Im_shift = reshape(shiftPatches(imS,:),floor(M/l1)+1,floor(N/l1)+1)';
+            for i = 1:floor(M/l1)+1
+                IM1 = [];
+                for j = 1:floor(N/l1)+1
+                    shift_optim = Im_shift(i,j);
+                    % extract patch in a tile
+                     imagePatche = imcrop(rgbIm,[sizVert(j) sizVert(i) l1-1 l1-1]);
+                    if j==floor(N/l1)+1 && i~=floor(N/l1)+1
+                       imagePatche = imcrop(rgbIm,[sizVert(j) sizVert(i)  N-sizVert(j)-1 l1-1]);
+                    elseif i==floor(N/l1)+1 && j~=floor(N/l1)+1
+                       imagePatche = imcrop(rgbIm,[sizVert(j) sizVert(i) l1-1  M-sizVert(i)-1]);
+                    elseif j==floor(N/l1)+1 && i==floor(N/l1)+1
+                       imagePatche = imcrop(rgbIm,[sizVert(j) sizVert(i) N-sizVert(j)-1   M-sizVert(i)-1]);
                     end
 
-                else
-                    %     % shift to the right
-                    shift = abs(shift);
-                    im1_shift = im_stripe;
-                    for rows = 2:2:size(im1_shift,1)
-                        im1_shift(rows,1:end-shift) = im_stripe(rows,shift+1:end);
-                    end
-
+                    rgbIm_shift =shiftImage(imagePatche,shift_optim);
+                    IM1 = cat(2,IM1,rgbIm_shift);
                 end
-                % rebuild the image but shifted
-                Im(:,start_stripe+1:stripes) = im1_shift;
-                start_stripe = stripes;
-                IM(:,:,i) = Im;
+                IMall{i,1} = IM1;
             end
+            RGBim1=cat(1,IMall{1:floor(N/l1)+1,1});
+        
+%         for i = 1:size(im,3)
+%             Im = im(:,:,i);
+%             Im_shift = Image_shift(:,i);
+%             Im_stripe_size = stripe_size(:,i);
+%             start_stripe = 0;
+%             for S = 1:length(Im_stripe_size)
+%                 stripes = Im_stripe_size(S);% no
+%                 shift = Im_shift(S);    
+%                 % take only part of the tile image due to different shifts
+%                 im_stripe = Im(:,start_stripe+1:stripes);
+%                 % shift to the left or right
+%                 if shift>=0
+%                     % shift to the left
+%                     im1_shift = im_stripe;
+%                     for rows = 2:2:size(im1_shift,1)
+%                         im1_shift(rows,shift+1:end) = im_stripe(rows,1:end-shift);
+%                     end
+% 
+%                 else
+%                     %     % shift to the right
+%                     shift = abs(shift);
+%                     im1_shift = im_stripe;
+%                     for rows = 2:2:size(im1_shift,1)
+%                         im1_shift(rows,1:end-shift) = im_stripe(rows,shift+1:end);
+%                     end
+% 
+%                 end
+%                 % rebuild the image but shifted
+%                 Im(:,start_stripe+1:stripes) = im1_shift;
+%                 start_stripe = stripes;
+             imShift(:,:,imS) = RGBim1;
         end
     
+    function im1_shift = shiftImage(Image, shift)
+
+       if shift>=0
+            % shift to the left
+            im1_shift = Image;
+            for rows = 2:2:size(im1_shift,1)
+                im1_shift(rows,shift+1:end) = Image(rows,1:end-shift);
+            end
+
+       else
+            %     % shift to the right
+            shift = abs(shift);
+            im1_shift = Image;
+            for rows = 2:2:size(im1_shift,1)
+                im1_shift(rows,1:end-shift) = Image(rows,shift+1:end);
+            end
+       end
+    %     
+    %     im(:,:,1) = im1_shift;
+    %     im(:,:,2) = im2_shift;
+    %     im(:,:,3) = im3_shift;
+
+%          im = cat(3, im1_shift,im2_shift,im3_shift);
+
+
 
